8d0236aecd7600063ac61f6386a02d1bd4524de9
Ntye <ntye.nina@gmail.com>
2024-04-24 02:56:02 +0200

    add

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css'
 import './App.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './router.jsx'
